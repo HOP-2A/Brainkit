794b4085b0d7f8cd57aae580e9fb1effb5a359de
25HP8413 <25HP8413@pinecones-iMac.local>
2025-11-27 17:55:25 +0800

    clerk-setup

---
+++ app/api/questionCreate/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/db";
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { title, creatorId, classroomId } = body;
+    if (!title)
+      return NextResponse.json({ error: "Title heregtei" }, { status: 401 });
+    else {
+      await prisma.quiz.create({
+        data: {
+          title,
+          creatorId,
+          classroomId,
+        },
+      });
+
+      return NextResponse.json(
+        { message: "successfully created quiz" },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: "aldaa garchlaa", error },
+      { status: 404 }
+    );
+  }
+}
